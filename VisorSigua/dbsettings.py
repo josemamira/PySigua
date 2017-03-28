c2a7ec17a66e0cc597074f1d2bc31b3fd58956a1
josemamira <josema.mira@gmail.com>
2017-03-28 20:20:07 +0200

    Delete dbsettings.py

--- VisorSigua/dbsettings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = '1.0.0'
-VERSION = tuple(map(int, __version__.split('.')))
-
-#params = ['172.25.224.197', "5432",'sigua', 'espacios', 'siguadmin']
-params = ['193.145.231.168', "5432",'sigua', 'espacios', 'siguadmin']
